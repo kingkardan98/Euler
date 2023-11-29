d2be086f6f471aadd966e74d8c72d3e18a010564
Francesco Bambina <f.bambina98@gmail.com>
2023-11-29 14:58:10 +0100

    Started solving P23

--- problem23.py
+++ problem23.py
@@ -5,7 +5,7 @@ def d(n):
     divSum = 0
     for i in range(2, int(math.sqrt(n) + 1)):
         if n % i == 0:
-            divSum += i 
+            divSum += i
             if i != n // i:
                 divSum += n // i
     return divSum + 1
@@ -24,10 +24,13 @@ for i in allNumbers:
         abundant.append(i)
 
 # Let's get the range of all numbers and remove all the sums of two abundant numbers.
+# Sum must absolutely be smaller than Sum(1^28123) n = 395465626
+# First result:                                        26190629   (Plausible but incorrect)
+
 for n in abundant:
-    for m in abundant[n:]:
-        if n+m in allNumbers:
-            print((n,m))
-            allNumbers.remove(n+m)
+    toRemove = [n+m for m in abundant if (m >= n and n+m < 28124)]
+    for el in toRemove:
+        if el in allNumbers:
+            pass
 
 print(sum(allNumbers))
