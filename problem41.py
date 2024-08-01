2e10bf10f7fba076d7811ae0bcb6b48f5c9b3096
Francesco Bambina <fr.bambina@gmail.com>
2024-08-01 15:24:25 +0200

    Solved problem 41

--- problem41.py
+++ problem41.py
@@ -5,6 +5,8 @@
 # 9-digit pandigital numbers, and seeing
 # which is the largest prime.
 
+# C1.1: 9-digit is a wrong assumption. Corrected later.
+
 # C2: selecting the pandigitals in [10^9, 10^10) is the wrong approach.
 # Way too long. How about permutations?
 
@@ -12,89 +14,48 @@
 # Now we still use the sieve of Erathostenes to get the primes,
 # for later ease of use of just looking in the list.
 
+# C4: sieves take up way too much memory. Pivoting to primality checks
+# and a yield function gave up memory.
+
 import itertools
+import math
 
 def generatePermutations(lst):
-    # The permutations get generated as a list of lists of the digits.
-    permutations_as_lists = list(itertools.permutations(lst))
+    # Yield each permutation as a concatenated integer
+    for permutation in itertools.permutations(lst):
+        perm_string = ''.join(map(str, permutation))
+        yield int(perm_string)
 
-    # This new list will contain the actual integers.
-    permutations = []
-
-    for permutation in permutations_as_lists:
-        # String where all the digits will be concatenated.
-        perm_string = ''
-
-        # The concatentation of all the actual digits.
-        for element in permutation:
-            perm_string += (str(element))
-
-        # Append the integer, instead of the string, for later
-        # ease of use.
-        permutations.append(int(perm_string))
-    
-    return permutations
-
-def sieveOfEratosthenes(n):
-    # Table of all numbers up to n
-    print("- Generating prime truth matrix table...")
-    prime_table = [True for _ in range(n + 1)]
-    print("- Done. Removing composite numbers...")
-
-    # 0 and 1 are not primes.
-    prime_table[0] = False
-    prime_table[1] = False
-
-    # The list of the actual numbers. Will fill later.
-    primes = []
-
-    # Prime factor to clear with
-    p = 2
-
-    # It is sufficient to search up to the last prime such that p^2 < UPPER_LIMIT,
-    # since for every n, if p is the smallest prime divisor of n, it is true that
-    # n = p * r >= p * p = p^2. So:
-
-    while (p * p <= n):
-        # If primes[p] is True, then it's prime
-        if prime_table[p]:
-            # Delete all multiples, starting from p^2, since all
-            # smaller numbers have already been taken care of.
-            # The step is p, so we're virtually multiplying.
-            for i in range(p * p, n + 1, p):
-                prime_table[i] = False
-        p += 1
-
-        # For aesthetic reasons.
-        if p % 10000 == 0:
-            print("-- Generated primes up to {}.\n".format(p))
-
-    # Create the list of prime numbers
-    for i in range(len(prime_table)):
-        if prime_table[i]:
-            primes.append(i)
-
-    return primes
+def isPrime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    for div in range(3, int(math.sqrt(number)) + 1, 2):
+        if number % div == 0:
+            return False
+    return True
 
 def main():
-    digitList = [i for i in range(1, 10)]
+    # 9-pandigital primes don't exists, since they're all divisible by 3.
+    # Similarly, 8-pandigital primes don't exists, since they're also divisble by 3.
+    # Best option: 7-pandigital
+    digitList = [i for i in range(1, 8)]
     print("Generating pandigitals...")
-    numList = generatePermutations(digitList)
-    print("Done. Generating primes...")
-    primes = sieveOfEratosthenes(10**10)
-    print("Done. Finding greatest pandigital...")
-    max = 0
+    
+    max_prime = 0
+    counter = 0
 
-    for number in numList:
-        if number % 2 == 0:
-            continue
-        if number not in primes:
-            continue
-        if number < max:
-            continue
-        max = number
+    for number in generatePermutations(digitList):
+        if counter % 1000 == 0 and counter != 0:
+            print("- Checked {} numbers".format(counter))
+        if isPrime(number) and number > max_prime:
+            max_prime = number
+        counter += 1
 
-    print(max)
+    print("Greatest pandigital prime:", max_prime)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
